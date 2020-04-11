1dcf8094a1b9a9d0470a14555830f2773c4ec159
Haneul Lee <lovesky4294@gmail.com>
2020-04-12 00:14:05 +0900

    *navigator routeName에서 header title 설정

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from '../screen/Movies'
 import Search from '../screen/Search'
@@ -7,11 +7,20 @@ import Tv from '../screen/Tv'
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-    <Tabs.Navigator>
+const getHeaderName = (route) => route ?.state ?.routeNames[route.state.index] || "Movies";
+
+export default ({ navigation, route }) => {
+    useLayoutEffect(() => {
+        // console.log(route)
+        navigation.setOptions({
+            title: getHeaderName(route)
+        });
+    }, [route]);
+
+    return (<Tabs.Navigator>
         <Tabs.Screen name='Movies' component={Movies}></Tabs.Screen>
-        <Tabs.Screen name='Favs' component={Favs}></Tabs.Screen>
+        <Tabs.Screen name='Favourites' component={Favs}></Tabs.Screen>
         <Tabs.Screen name='Search' component={Search}></Tabs.Screen>
-        <Tabs.Screen name='Tv' component={Tv}></Tabs.Screen>
-    </Tabs.Navigator>
-)
+        <Tabs.Screen name='TV' component={Tv}></Tabs.Screen>
+    </Tabs.Navigator>)
+}
